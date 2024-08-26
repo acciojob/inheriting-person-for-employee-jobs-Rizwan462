222ca2fe19f41f296eb027c7582dc910cfab5da7
Rizwan462 <139914755+Rizwan462@users.noreply.github.com>
2024-08-26 23:07:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ function Employee(name, age, jobTitle) {}
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+
+class Person{
+	constructor(name, age){
+		this.name = name;
+		this.age = age;
+	}
+	printPerson(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+	}
+}
+
+class Employee extends Person{
+	constructor(name, age, jobTitle){
+		super(name, age);
+		this.jobTitle = jobTitle;
+	}
+	printEmp(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+	}
+}
+
+const emp1 = Employee("Alice", 25, "Manager");
+
+emp1.printPerson()
+emp1.printEmp()
+
+
