def977475249b48a517e8fe98cd1be3888eb3f5d
Rizwan462 <139914755+Rizwan462@users.noreply.github.com>
2024-08-26 23:38:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// complete this js code
+function Person(name, age) {}
+
+function Employee(name, age, jobTitle) {}
+
+// Do not change code below this line
+window.Person = Person;
+window.Employee = Employee;
 class Person{
 	constructor(name, age){
 		this.name = name;
