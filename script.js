f0cadc12ec02d56ddf55eee01c870ead1f6c348c
Rizwan462 <139914755+Rizwan462@users.noreply.github.com>
2024-08-26 23:50:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,18 @@
-// complete this js code
 function Person(name, age) {
-	class Person{
-	constructor(name, age){
-		this.name = name;
-		this.age = age;
-	}
-	greet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
-	}
-}}
+    this.name = name;
+    this.age = age;
+    this.greet = function() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    }
+}
 
 function Employee(name, age, jobTitle) {
-	class Employee extends Person{
-	constructor(name, age, jobTitle){
-		super(name, age);
-		this.jobTitle = jobTitle;
-	}
-	jobGreet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
-	}
+    Person.call(this, name, age);
+    this.jobTitle = jobTitle;
+    this.jobGreet = function() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    }
 }
-}
-
-
-
-
-
-
-const emp1 = new Employee("Alice", 25, "Manager");
-
-emp1.greet();
-emp1.jobGreet();
 
 // Do not change code below this line
 window.Person = Person;
