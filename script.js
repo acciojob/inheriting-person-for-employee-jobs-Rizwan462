275bb612e2c32fd92be0cd251e0068e27c86c5cd
Rizwan462 <139914755+Rizwan462@users.noreply.github.com>
2024-08-26 23:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 // complete this js code
-
-
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
-class Person{
+function Person(name, age) {
+	class Person{
 	constructor(name, age){
 		this.name = name;
 		this.age = age;
@@ -12,9 +8,10 @@ class Person{
 	greet(){
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
 	}
-}
+}}
 
-class Employee extends Person{
+function Employee(name, age, jobTitle) {
+	class Employee extends Person{
 	constructor(name, age, jobTitle){
 		super(name, age);
 		this.jobTitle = jobTitle;
@@ -23,8 +20,18 @@ class Employee extends Person{
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 	}
 }
+}
+
+
+
+
+
 
 const emp1 = new Employee("Alice", 25, "Manager");
 
 emp1.greet();
 emp1.jobGreet();
+
+// Do not change code below this line
+window.Person = Person;
+window.Employee = Employee;
