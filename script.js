b14453ba41e52b3b0d787a6f72cb1e0dff568619
Rizwan462 <139914755+Rizwan462@users.noreply.github.com>
2024-08-28 17:16:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+}
+Person.prototype.greet = function(){
+	console.log(`Hello, my name is ${this.name} I am ${this.age} years old.`);
+};
 
-function Employee(name, age, jobTitle) {}
-class Person{
-	constructor(name, age){
-		this.name = name;
-		this.age = age;
-	}
-	greet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
-	}
+function Employee(name, age, jobTitle) {
+	Person.call(this, name, age);
+	this.jobTitle = jobTitle;
+}
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function (){
+	console.log(`Hello, my name is ${this.name} I am ${this.age} years old, and my job title is ${this.jobTitle}`);
 }
 
-class Employee extends Person{
-	constructor(name, age, jobTitle){
-		super(name, age);
-		this.jobTitle = jobTitle;
-	}
-	jobGreet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
-	}
-}
 
 const emp1 = new Employee("Alice", 25, "Manager");
 
